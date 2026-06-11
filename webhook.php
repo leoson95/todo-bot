13ec1c2e7690bf96250bffe659052725957f6a83
leoson95 <40212922+leoson95@users.noreply.github.com>
2026-06-11 18:30:08 +0330

    Initial PHP webhook structure

---
+++ webhook.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/vendor/autoload.php';
+
+use TelegramBot\TelegramBot;
+
+// TODO: Load config and initialize bot
+
+echo 'TodoBot PHP Webhook is ready';
